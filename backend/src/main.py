27487a47fa4303f53ad2aa0007f52dd5caa0b5e4
Pedro Rodrigues <peddroprp@gmail.com>
2020-08-31 21:38:29 -0300

    Api basic structure

---
+++ backend/src/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app, automatic_options=True)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8083)
